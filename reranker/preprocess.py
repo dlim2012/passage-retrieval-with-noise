909b2779e9c408ef82e7347f31329b335f97c000
dlim2012 <57515449+dlim2012@users.noreply.github.com>
2022-04-02 00:33:05 -0400

    Update preprocess.py
    Added comments

--- reranker/preprocess.py
+++ reranker/preprocess.py
@@ -3,69 +3,67 @@ from transformers import BertTokenizer
 import os
 import numpy as np
 
+
 # Directory to save preprocessed data
 data_dir = 'Data/MS_Marco/rerank/'
 
-# Download MS Marco dataset
-ms_marco = load_dataset('ms_marco', 'v2.1')
-
-# Model name
+# Model name of the pre-trained BERT
 model_name = 'bert-base-uncased'
 
-# BERT Tokenizer from HuggingFace
-tokenizer = BertTokenizer.from_pretrained(model_name) # WordPiece tokenizer
 
+if __name__ == '__main__':
 
-for dataset_type in ['train', 'validation']:
+    # BERT Tokenizer from HuggingFace
+    tokenizer = BertTokenizer.from_pretrained(model_name) # WordPiece tokenizer
 
-    ms_marco_type = ms_marco[dataset_type]
-    n = len(ms_marco_type) # n_train: 808731, n_validation: 101093
+    # Download MS Marco dataset
+    ms_marco = load_dataset('ms_marco', 'v2.1')
 
-    ###############################################################################
-    #if dataset_type == 'train':
-    #    n = 100
-    #else:
-    #    n = 10
-    ###############################################################################
+    # Preprocess both train and validation data
+    for dataset_type in ['train', 'validation']:
 
-    input_ids = []
-    labels = []
+        ms_marco_type = ms_marco[dataset_type]
+        n = len(ms_marco_type) # n_train: 808731, n_validation: 101093
 
-    for i in range(n):
+        # lists to save intermediatory results
+        input_ids = []
+        labels = []
 
-        # Truncate query to have at most 64 tokens
-        query_tokens = tokenizer.tokenize(ms_marco_type[i]['query'])[:64]
+        for i in range(n):
 
-        # Convert tokens of query to token ids
-        query_token_ids = tokenizer.convert_tokens_to_ids(query_tokens)
+            # Truncate query to have at most 64 tokens
+            query_tokens = tokenizer.tokenize(ms_marco_type[i]['query'])[:64]
 
-        query_len = len(query_token_ids)
+            # Convert tokens of query to token ids
+            query_token_ids = tokenizer.convert_tokens_to_ids(query_tokens)
 
-        passages = ms_marco_type[i]['passages']['passage_text']
+            # All passages that will be used with the query 
+            passages = ms_marco_type[i]['passages']['passage_text']
 
-        for passage in passages:
+            for passage in passages:
 
-            # Truncate passage so that [<CLS> query <SEP> passage <SEP>] doesn't exceed 512
-            passage_tokens = tokenizer.tokenize(passage)[:509 - query_len]
+                # Truncate passage so that [<CLS> query <SEP> passage <SEP>] doesn't exceed 512
+                passage_tokens = tokenizer.tokenize(passage)[:509 - len(query_token_ids)]
 
-            # Convert tokens of passage to token ids
-            passage_token_ids = tokenizer.convert_tokens_to_ids(passage_tokens)
+                # Convert tokens of passage to token ids
+                passage_token_ids = tokenizer.convert_tokens_to_ids(passage_tokens)
 
-            # token ids for input
-            token_ids = [101] + query_token_ids + [102] + passage_token_ids + [102]
+                # Token ids for input
+                token_ids = [101] + query_token_ids + [102] + passage_token_ids + [102]
 
-            # pad token ids to use batch
-            token_ids += [0] * (512 - len(token_ids))
+                # Pad token ids to use batch
+                token_ids += [0] * (512 - len(token_ids))
 
-            input_ids.append(token_ids)
+                input_ids.append(token_ids)
+     
+            labels += ms_marco_type[i]['passages']['is_selected']
 
-        labels += ms_marco_type[i]['passages']['is_selected']
+        # Paths to save input_ids and labels
+        input_ids_save_path = os.path.join(data_dir, '%s_input_ids.npy' % dataset_type)
+        labels_save_path = os.path.join(data_dir, '%s_labels.npy' % dataset_type)
 
-
-    input_ids_save_path = os.path.join(data_dir, '%s_input_ids.npy' % dataset_type)
-    labels_save_path = os.path.join(data_dir, '%s_labels.npy' % dataset_type)
-
-    np.save(input_ids_save_path, input_ids)
-    np.save(labels_save_path, labels)
+        # Save the results
+        np.save(input_ids_save_path, input_ids)
+        np.save(labels_save_path, labels)
 
 
