9f640bb661211ee3da672d61e3be784add00c4b8
dlim2012 <57515449+dlim2012@users.noreply.github.com>
2022-04-02 00:32:37 -0400

    Update model.py
    Fixed a mistake: using only the <CLS> vector instead of all output vector of BERT
    Added comments

--- reranker/model.py
+++ reranker/model.py
@@ -5,37 +5,64 @@ import pytorch_lightning as pl
 class Reranker(pl.LightningModule):
     def __init__(self, model_name='bert-base-uncased'):
         super().__init__()
+
+        # Download a pre-trained BERT model
         self.bert = BertModel.from_pretrained(model_name)
+
+        # Linear layer that will use the <CLS> vector as its input
         self.linear = torch.nn.Linear(in_features=768, out_features=2, bias=True)
+
+        # Cross-entropy loss
         self.criterion = torch.nn.CrossEntropyLoss()
         
     def forward(self, x):
+        """
+        :param x: x['input_ids'] is the input to the BERT
+        """
         
-        input_ids = x['input_ids']
-        output = self.bert(input_ids)
-        output = output.last_hidden_state
+        # Calculate the last hidden layer of the output
+        output = self.bert(x['input_ids']).last_hidden_state
+
+        # Use the <CLS> vector only
+        output = output[:, 0, :]
+        
+        # Linear layer
         output = self.linear(output)
+
+
         return output
     
     def training_step(self, batch, batch_idx):
+        
+        # Compute the forward path
         output = self.forward(batch)
+
+        # Compute the cross-entropy loss
         loss = self.criterion(output, batch['labels'])
         
-        
+        # Log loss
         self.log('loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         
+        # Return loss for backpropagation
         return {'loss': loss}
         
     def validation_step(self, batch, batch_idx):
+
+        # Compute the forward path
         output = self.forward(batch)
+
+        # Compute the cross-entropy loss
         loss = self.criterion(output, batch['labels'])
         
+        # Log loss
         self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         
-        return {'loss': loss}
+
+        #return {'loss': loss}
     
     def configure_optimizers(self):
         
+        # Using Adam optimizer as shown in the paper (lr=3e-6, beta1=0.9, beta2=0.999, L2_weight_decay=0.01)
         optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, self.parameters()), lr=3e-6, weight_decay=0.01)
         
         return optimizer
