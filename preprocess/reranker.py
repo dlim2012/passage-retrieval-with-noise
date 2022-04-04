daf6731839368eaa38a1dbc076c75946e7b68829
dlim2012 <57515449+dlim2012@users.noreply.github.com>
2022-04-04 15:07:16 -0400

    Update reranker.py
    minor changes.

--- preprocess/reranker.py
+++ preprocess/reranker.py
@@ -9,7 +9,9 @@ train:
         save 800000 in each file -- total 16
         input ids(length 512) > 'train_input_ids_npr4_{}.npy'
         labels > 'train_labels_npr4_{}.npy'
-
+    merge npy files
+        'train_input_ids_npr4_{}.npy' > 'train_input_ids_npr4_all.npy'
+        'train_labels_npr4_{}.npy' > 'train_labels_npr4_all.npy'
     
 validation: all pairs in top1000
     'qid pid label' > 'validation.qidpidlabel.tsv'
@@ -17,6 +19,9 @@ validation: all pairs in top1000
         save 800000 in each file -- total 16
         input ids(length 512) > 'validation_input_ids_npr4_{}.npy'
         labels > 'validation_labels_npr4_{}.npy'
+    merge npy files
+        'validation_input_ids_npr4_{}.npy' > 'validation_input_ids_npr4_all.npy'
+        'validation_labels_npr4_{}.npy' > 'validation_labels_npr4_all.npy'
 """
 
 """
@@ -37,21 +42,23 @@ from tools.paths import *
 from tools.tools import shuffle_text, one_hot_vector
 from tools.read_files import read_texts, read_qidpidtriples, read_top1000_npy, read_qrels
 
-input_ids_file_name = 'reranker/{dataset_type}_input_ids_npr{neg_per_pos}_{save_number}.npy'
-labels_file_name = 'reranker/{dataset_type}_labels_npr{neg_per_pos}_{save_number}.npy'
+save_file_name = 'reranker/{dataset_type}_{to_save}_npr{neg_per_pos}_{save_number}.npy'
 
 # Model name of the pre-trained BERT
 model_name = 'bert-base-uncased'
 
 # negative - positive ratio
 neg_per_pos = 4
+ratio = 1 / neg_per_pos
 
 # Save every {} data instance (due to memory issue)
 save_size = 800000
 max_size = 12800000
 
+########################################################################
 #save_size = 100
-#max_size = 1000
+#max_size = 300
+########################################################################
 
 def format_input_ids(query, passage, tokenizer):
     """
@@ -80,7 +87,45 @@ def format_input_ids(query, passage, tokenizer):
     
     return token_ids
 
-def save_in_npy(qidpidlabel_file, queries, passages, tokenizer, dataset_type):
+def merge_npy_files(file_name, dataset_type, neg_per_pos, save_number):
+    print("\tMerging npy files...")
+    
+    input_ids = []
+    labels = []
+    
+    for i in range(save_number):
+        input_ids_path = os.path.join(write_dir, file_name.format(
+            dataset_type=dataset_type,
+            to_save='input_ids',
+            neg_per_pos=neg_per_pos,
+            save_number=i))
+        labels_path = os.path.join(write_dir, file_name.format(
+            dataset_type=dataset_type,
+            to_save='labels',
+            neg_per_pos=neg_per_pos,
+            save_number=i))
+        
+        
+        input_ids.append(np.load(input_ids_path))
+        labels.append(np.load(labels_path))
+        
+        print('\t\t%d' % i, input_ids[-1].shape, labels[-1].shape)
+        
+    input_ids_save_path = os.path.join(write_dir, file_name.format(
+        dataset_type=dataset_type,
+        to_save='input_ids',
+        neg_per_pos=neg_per_pos,
+        save_number='all'))
+    labels_save_path = os.path.join(write_dir, file_name.format(
+        dataset_type=dataset_type,
+        to_save='labels',
+        neg_per_pos=neg_per_pos,
+        save_number='all'))
+    
+    np.save(input_ids_save_path, np.concatenate(input_ids).astype(np.int16))
+    np.save(labels_save_path, np.concatenate(labels).astype(np.int8))
+        
+def save_in_npy(qidpidlabel_file, save_file_name, queries, passages, tokenizer, dataset_type):
     """
     Given a 'qid pid label' file, make and save data for reranker
     input_ids file: reranker/{dataset_type}_input_ids_npr{neg_per_pos}_{save_number}.npy
@@ -88,9 +133,9 @@ def save_in_npy(qidpidlabel_file, queries, passages, tokenizer, dataset_type):
     
     """
     
-    
     print("\tsave_size: %d, max_size: %d" % (save_size, max_size))
 
+    num_lines = sum(1 for line in open(qidpidlabel_file, 'r'))
     read_file = open(qidpidlabel_file, 'r')
     reader = csv.reader(read_file, delimiter='\t')
     
@@ -99,6 +144,7 @@ def save_in_npy(qidpidlabel_file, queries, passages, tokenizer, dataset_type):
     
     # Save number for each file
     save_number = 0
+    
                              
     for i, line in enumerate(reader):
                              
@@ -110,24 +156,27 @@ def save_in_npy(qidpidlabel_file, queries, passages, tokenizer, dataset_type):
                                      
         
         # Save every save_step
-        if (i+1) % save_size == 0:
+        if (i+1) % save_size == 0 or i == num_lines - 1:
             
             # Path to save npy files
-            input_ids_save_path = os.path.join(write_dir, input_ids_file_name.format(
+            input_ids_save_path = os.path.join(write_dir, save_file_name.format(
                 dataset_type=dataset_type,
+                to_save='input_ids',
                 neg_per_pos=neg_per_pos,
                 save_number=save_number))
-            labels_save_path = os.path.join(write_dir, labels_file_name.format(
+            labels_save_path = os.path.join(write_dir, save_file_name.format(
                 dataset_type=dataset_type,
+                to_save='labels',
                 neg_per_pos=neg_per_pos,
                 save_number=save_number))
+            
            
             # Check overflow
             assert np.max(input_ids) <= 32767
 
             # Save the results
             np.save(input_ids_save_path, np.array(input_ids, dtype=np.int16)) # np.int16: -32_768 to 32_767
-            np.save(labels_save_path, one_hot_vector(labels).astype(np.int8)) # np.int8: -128 to 127
+            np.save(labels_save_path, one_hot_vector(labels, 2).astype(np.int8)) # np.int8: -128 to 127
             
             print("\t%s, %s saved." % (input_ids_save_path, labels_save_path))
             
@@ -139,14 +188,16 @@ def save_in_npy(qidpidlabel_file, queries, passages, tokenizer, dataset_type):
         if (i+1) == max_size:
             break
             
+        
+            
     read_file.close()
     
+    merge_npy_files(save_file_name, dataset_type, neg_per_pos, save_number)
+    
     
 
 
-def preprocess_train_from_qidpidtriples(tokenizer, passages):
-    # Make write directory if not exists
-    os.makedirs(write_dir, exist_ok=True)
+def preprocess_train_from_qidpidtriples(save_file_name, tokenizer, passages):
     
     # Shuffle 'qidpidtriples.train.full.txt' if not shuffled
     if not os.path.exists(qidpidtriples_shuffled_file):
@@ -171,11 +222,15 @@ def preprocess_train_from_qidpidtriples(tokenizer, passages):
             qid, pid1, pid2 = map(int, line)
 
             # Add a positive pair with pre-defined probability
-            if random.random() < 1/neg_per_pos:
+            if random.random() < ratio:
                 write_file.write('%d\t%d\t%d\n' % (qid, pid1, 1))
 
             # Add a negative pair every step
             write_file.write('%d\t%d\t%d\n' % (qid, pid2, 0))
+            
+            #######################################################################
+            #if i == 500: break
+            #######################################################################
             
         read_file.close()
         write_file.close()
@@ -188,36 +243,45 @@ def preprocess_train_from_qidpidtriples(tokenizer, passages):
     # Read queries and passages 
     queries = read_texts(train_query_file)
     
-    save_in_npy(train_qidpidlabel_file, queries, passages, tokenizer, 'train')
+    save_in_npy(train_qidpidlabel_file, save_file_name, queries, passages, tokenizer, 'train')
     
-
     
-def preprocess_validation(tokenizer, passages, top1000, positives, qidpidlabel_file, dataset_type):
+def prerprocess_train_from_top1000(save_file_name, tokenizer, passages, top1000, positives, qidpidlabel_file, dataset_type):
     """
     From top1000 make all possible qidpidlabel triples for validation dataset
     positives: (dict)[qid] = [relevant pids]
     qidpidlabel_file: 'qid\tpid\tlabel' format
     dataset_type: used forfile name 'train', 'validation'
     """
+    
     if not os.path.exists(qidpidlabel_file):
         print("\tWriting to %s..." % qidpidlabel_file)
     
         lines = []
 
-        for qid in top1000:
-
-            pids = top1000[qid]
-
-            for pid in pids:
-                if pid == -1:
-                    break
-
-                if pid in positives[qid]:
-                    label = 1
-                else:
-                    label = 0
-
-                lines.append('%d\t%d\t%d\n' %(qid, pid, label))
+        for i, qid in enumerate(top1000):
+            
+            for pid1 in positives[qid]:
+                
+                lines.append('%d\t%d\t%d\n' %(qid, pid1, label))
+                
+                neg_pids = []
+                
+                for j in range(1000):
+                    
+                    pid2 = random.choice(top1000[qid])
+                    
+                    if pid2 == -1 or pid2 in positives[qid] or pid2 in neg_pids:
+                        continue
+                        
+                    neg_pids.append(pid2)
+                    
+                    if len(neg_pids) >= 4:
+                        break
+                
+                for pid2 in neg_pids:
+                    
+                    lines.append('%d\t%d\t%d\n' %(qid, pid2, label))
 
         print("\t\tShuffling lines...")
         random.shuffle(lines)
@@ -230,7 +294,61 @@ def preprocess_validation(tokenizer, passages, top1000, positives, qidpidlabel_f
     
     queries = read_texts(dev_query_file)
     
-    save_in_npy(valid_qidpidlabel_file, queries, passages, tokenizer, dataset_type)
+    save_in_npy(valid_qidpidlabel_file, save_file_name, queries, passages, tokenizer, dataset_type)
+    pass
+    
+
+    
+def preprocess_validation_from_top1000(save_file_name, tokenizer, passages, top1000, positives, qidpidlabel_file, dataset_type):
+    """
+    From top1000 make all possible qidpidlabel triples for validation dataset
+    top1000: read top 1000 using tools.read_files.read_top1000_npy
+    positives: (dict)[qid] = [relevant pids]
+    dataset_type: used for file name 'train', 'validation'
+    
+    qidpidlabel_file: the path to this file
+        Will make the file in this function if not exist ('qid\tpid\tlabel' format)
+    """
+    
+    if not os.path.exists(qidpidlabel_file):
+        print("\tWriting to %s..." % qidpidlabel_file)
+    
+        lines = []
+
+        for i, qid in enumerate(top1000):
+
+            pids = top1000[qid]
+
+            for pid in pids:
+                if pid == -1:
+                    break
+
+                if pid in positives[qid]:
+                    label = 1
+                else:
+                    label = 0                    
+
+                lines.append('%d\t%d\t%d\n' %(qid, pid, label))
+
+            #######################################################################
+            #if i == 1000: break
+            #######################################################################
+
+        print("\t\tShuffling lines...")
+        random.shuffle(lines)
+
+        with open(qidpidlabel_file, 'w') as f:
+            f.writelines(lines)
+    else:
+        print("\t%s exists." % qidpidlabel_file)
+    
+    
+    queries = read_texts(dev_query_file)
+    
+    save_in_npy(valid_qidpidlabel_file, save_file_name, queries, passages, tokenizer, dataset_type)
+    
+    
+    
     
 def main():
     
@@ -246,13 +364,21 @@ def main():
     # Preprocess reranking (label: irrelevant(0), relevant(1))
     print("Preprocess reranking training data... (label: relevance)")
     print("\tneg_per_pos: %d" % neg_per_pos)
-    preprocess_train_from_qidpidtriples(tokenizer, passages)
+    preprocess_train_from_qidpidtriples(save_file_name, tokenizer, passages)
     
     # Preprocess reranking (label: irrelevant(0), relevant(1))
     print("Preprocess reranking validation data... (label: relevance)")
     top1000 = read_top1000_npy(top1000_dev_id_file)
     positives = read_qrels(dev_qrels_file)
-    preprocess_validation(tokenizer, passages, top1000, positives, valid_qidpidlabel_file, 'validation')
+    preprocess_validation_from_top1000(save_file_name,
+                                       tokenizer,
+                                       passages,
+                                       top1000,
+                                       positives,
+                                       valid_qidpidlabel_file,
+                                       'validation')
+    
+    
     
     
     # TODO: preprocess reranking with top10 as label
@@ -261,4 +387,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+    #merge_npy_files('reranker_0402/{dataset_type}_{to_save}_npr{neg_per_pos}_{save_number}.npy', 'train', neg_per_pos, 16)
+    #merge_npy_files('reranker_0402/{dataset_type}_{to_save}_npr{neg_per_pos}_{save_number}.npy', 'validation', neg_per_pos, 8)
     
