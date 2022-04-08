3c983836005d603b39578b79b9731fd4f14f2915
dlim2012 <57515449+dlim2012@users.noreply.github.com>
2022-04-08 19:49:32 -0400

    Create model.py
    Initial uploads.
    Issue:
        To avoid probability from becoming 0.0000+e00, the scores are divided by 10 before the softmax layer

---
+++ dpr/model.py
@@ -0,0 +1,165 @@
+import torch
+from transformers import BertModel, get_linear_schedule_with_warmup
+import pytorch_lightning as pl
+import numpy as np
+
+
+class DPR(pl.LightningModule):
+    def __init__(self, linear_scheduler_steps, B, model_name='bert-base-uncased'):
+        super().__init__()
+
+        self.query_encoder = BertModel.from_pretrained(model_name)
+        self.passage_encoder = BertModel.from_pretrained(model_name)
+
+
+        self.linear_scheduler_steps = linear_scheduler_steps
+        self.B = B
+
+        #self.labels = torch.cat((torch.eye(B), torch.zeros(B, B)), 1)
+        self.softmax = torch.nn.Softmax(dim=1)
+        self.labels = torch.arange(B)
+
+        # Monitor training steps for logging purpose
+        self.steps = torch.tensor(0).type(torch.float32)
+
+        # counts and step intervals for performance logging
+        self.counts = np.zeros(4) # n_correct, n_total, n_correct_epoch, n_total_epoch
+        self.loss_interval = torch.tensor(0).type(torch.float32)
+        self.measure_steps = 1000
+
+    def forward(self, x):
+        """
+        :param x: x['input_ids'] is the input to the BERT
+        """
+
+        #print(x['query_input_ids'].shape, x['query_attention_mask'].shape, x['positive_input_ids'].shape,
+        #      x['positive_attention_mask'].shape, x['negative_input_ids'].shape, x['negative_attention_mask'].shape)
+
+        query_vectors = self.query_encoder(
+            input_ids=x['query_input_ids'],
+            attention_mask=x['query_attention_mask']
+        ).last_hidden_state[:, 0, :]    # shape: (B, d)
+
+        positive_vectors = self.passage_encoder(
+            input_ids=x['positive_input_ids'],
+            attention_mask=x['positive_attention_mask']
+        ).last_hidden_state[:, 0, :]    # shape: (B, d)
+
+        negative_vectors = self.passage_encoder(
+            input_ids=x['negative_input_ids'],
+            attention_mask=x['negative_attention_mask']
+        ).last_hidden_state[:, 0, :]    # shape: (B, d)
+
+        passage_vectors = torch.cat((positive_vectors, negative_vectors), 0) # shape: (2B, d)
+
+        # similarity scores
+        scores = torch.matmul(query_vectors, passage_vectors.T) # shape: (B, 2B)
+
+        return scores
+
+    def loss(self, scores):
+        scores /= 10  # Divided by 10 to avoid having 0.0000e+00 for some of the softmax result
+
+        # Calculate the probability of predicting correctly
+        x = torch.sum(torch.softmax(scores, dim=1)[:, :self.B] * torch.eye(self.B), axis=1)
+
+        # Calculate the negative log likelihood
+        x = -1 * torch.log(x)
+
+        # Calculate the mean loss
+        loss = torch.mean(x)
+
+        return loss
+
+
+    def training_step(self, batch, batch_idx):
+        self.steps += 1
+
+        # Compute the forward path
+        scores = self.forward(batch)
+        scores = scores.cpu()
+        loss = self.loss(scores)
+
+        self.add_counts(scores)
+        self.loss_interval += loss
+
+        # Save log
+        self.log('loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log('steps', self.steps, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+
+        if self.steps % self.measure_steps == 0:
+            accuracy = self.counts[0] / self.counts[1]
+            self.loss_interval /= self.measure_steps
+            self.log('acc', accuracy, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+            self.log('loss_interval', self.loss_interval, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+            print(accuracy, self.loss_interval)
+            self.counts[:2] = 0
+            self.loss_interval = torch.tensor(0).type(torch.float32)
+
+        # Return loss for backpropagation
+        return {'loss': loss}
+
+    def train_epoch_end(self, outputs):
+        accuracy = self.counts[0] / (self.counts[1] + 1e-10)
+        accuracy_epoch = self.counts[2] / self.counts[3]
+        self.log('acc', accuracy, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log('acc_end', accuracy_epoch, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.counts = np.zeros(4)
+
+    def validation_step(self, batch, batch_idx):
+
+        # Compute the forward path
+        scores = self.forward(batch)
+        loss = self.loss(scores)
+
+        self.add_counts(scores)
+
+        # Save log
+        self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+
+        if self.steps % self.measure_steps == 0:
+            accuracy = self.counts[0] / self.counts[1]
+            self.log('val_acc', accuracy, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+            self.counts[:2] = 0
+
+    def validation_epoch_end(self, outputs):
+        accuracy = self.counts[0] / (self.counts[1] + 1e-10)
+        accuracy_epoch = self.counts[2] / self.counts[3]
+        self.log('val_acc', accuracy, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log('val_acc_end', accuracy_epoch, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.counts = np.zeros(4)
+
+    def configure_optimizers(self):
+
+        # Using Adam optimizer
+        # Configuration as in the paper: lr=3e-6, beta1=0.9, beta2=0.999, L2_weight_decay=0.01
+        optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, self.parameters()), lr=3e-6, weight_decay=0.01)
+
+        # Configuration as in the paper: learning rate warmup over the first 10,000 steps
+        schedulers = [
+            {
+                'scheduler': get_linear_schedule_with_warmup(
+                    optimizer,
+                    num_warmup_steps=self.linear_scheduler_steps[0],
+                    num_training_steps=self.linear_scheduler_steps[1]
+                ),
+                'name': 'warm_up_lr',
+                'interval': 'step'
+            }
+        ]
+
+        # Difference from the paper: linear decay of learning rate is not applied and we use smaller batch size
+
+        return [optimizer], schedulers
+
+    def add_counts(self, scores):
+
+        prediction = torch.argmax(scores, axis=1)
+        n_correct = torch.sum((prediction == self.labels).type(torch.int32)).tolist()
+        n_total = scores.shape[0]
+
+        self.counts[[0, 2]] += n_correct
+        self.counts[[1, 3]] += n_total
+
+
+
