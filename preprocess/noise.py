1c20c4c25c05132c6c322b0d71e972538778298a
dlim2012 <57515449+dlim2012@users.noreply.github.com>
2022-04-15 16:51:48 -0400

    Update noise.py
    Added comments and removed unnecessary lines.

--- preprocess/noise.py
+++ preprocess/noise.py
@@ -17,21 +17,23 @@ from tqdm import tqdm
 
 noiseLevel = 0.1 # Change amount of noise generated here (Percentage of words in input, not chars)
 
+# Set the reading and saving directories
 read_dir = 'data/ms_marco/original'
-
-queries_train_filename = 'queries.train'
-queries_dev_filename = 'queries.dev'
-passages_filename = 'collection'
-
-queries_train = pd.read_csv(os.path.join(read_dir, '%s.tsv' % queries_train_filename), sep='\t', header = None)
-queries_dev = pd.read_csv(os.path.join(read_dir, '%s.tsv' % queries_dev_filename), sep='\t', header = None)
-collection = pd.read_csv(os.path.join(read_dir, '%s.tsv' % passages_filename), sep='\t', header = None)
-
-#save_dir = 'data/ms_marco/preprocessed/noisy'
 save_dir = '/mnt/ssd/noisy'
 os.makedirs(save_dir, exist_ok=True)
 
+# file names to use: ('%s.tsv' % file_name)
+queries_dev_filename = 'queries.dev'
+passages_filename = 'collection'
+
+# read data
+queries_dev = pd.read_csv(os.path.join(read_dir, '%s.tsv' % queries_dev_filename), sep='\t', header = None)
+collection = pd.read_csv(os.path.join(read_dir, '%s.tsv' % passages_filename), sep='\t', header = None)
+
 def word_order_swap(text, adjacent=False):
+    """
+    swap word order
+    """
     sentences = text.split('.')
     for i in range(len(sentences)):
         words = sentences[i].split(' ')
@@ -48,6 +50,9 @@ def word_order_swap(text, adjacent=False):
 
 
 def generateNoise(outputfileName, inputData, transformationType, desc):
+    """
+    generate noise
+    """
     print('###### Start: %s #######' % outputfileName)
     augmenter = Augmenter(transformation=transformationType, pct_words_to_swap=noiseLevel)
     for x in tqdm(range(0, inputData.shape[0]), desc=desc):
@@ -59,57 +64,29 @@ def generateNoise(outputfileName, inputData, transformationType, desc):
     inputData.to_csv(outputfileName, sep = '\t', header = None, index = False)
     print('###### Done: %s #######' % outputfileName)
 
-
+# file names and readers
 files = {
-    queries_train_filename: queries_train,
     queries_dev_filename: queries_dev,
     passages_filename: collection
 }
 
-
+# transformation types
 transformations = {
-    #'BackTranslation': BackTranslation(), # This takes too long and seem to cause network disconnection
-    'WordSwapInflections': WordSwapInflections(), # This works
-    #'WordSwapChangeLocation': WordSwapChangeLocation(), # This literally changes location names and not word orders
-    'WordSwapRandomCharacterInsertion': WordSwapRandomCharacterInsertion(), # This works
-    'WordSwapRandomCharacterDeletion': WordSwapRandomCharacterDeletion(), # This works
-    'WordSwapRandomCharacterSubstitution': WordSwapRandomCharacterSubstitution(), # This works
-    'WordSwapNeighboringCharacterSwap': WordSwapNeighboringCharacterSwap(), # This works
-    #'WordSwapQWERTY': WordSwapQWERTY(), # This gives error (IndexError: Cannot choose from an empty sequence)
+    'WordSwapInflections': WordSwapInflections(),
+    'WordSwapRandomCharacterInsertion': WordSwapRandomCharacterInsertion(),
+    'WordSwapRandomCharacterDeletion': WordSwapRandomCharacterDeletion(),
+    'WordSwapRandomCharacterSubstitution': WordSwapRandomCharacterSubstitution(),
+    'WordSwapNeighboringCharacterSwap': WordSwapNeighboringCharacterSwap(),
     'word_order_swap': word_order_swap
 }
 
-transformations_keys = [
-    #'WordSwapQWERTY',
-    'word_order_swap',
-    'WordSwapNeighboringCharacterSwap',
-    'WordSwapRandomCharacterSubstitution',
-    'WordSwapRandomCharacterDeletion',
-    'WordSwapRandomCharacterInsertion',
-    #'WordSwapChangeLocation',
-    'WordSwapInflections',
-    #'BackTranslation'
-]
-
 def main():
-    files_keys = [queries_dev_filename, passages_filename]#, queries_train_filename]
 
-    transformations_keys = [
-        # 'WordSwapQWERTY',
-        'word_order_swap',
-        'WordSwapNeighboringCharacterSwap',
-        'WordSwapRandomCharacterSubstitution',
-        'WordSwapRandomCharacterDeletion',
-        'WordSwapRandomCharacterInsertion',
-        # 'WordSwapChangeLocation',
-        'WordSwapInflections',
-        # 'BackTranslation'
-    ]
-
-    #with ThreadPoolExecutor(max_workers=10) as executor:
+    # Generate noise using a process pool.
+    # (Somehow this doesn't use most of available CPUs. Perhaps should use Multiprocessing.Pool instead)
     with ProcessPoolExecutor(10) as executor:
-        for filename in files_keys:
-            for transformationType in transformations_keys:
+        for filename in files.keys():
+            for transformationType in transformations.keys():
                 save_path = os.path.join(save_dir, '%s.%s_%.2f.tsv' % (filename, transformationType, noiseLevel))
                 executor.submit(generateNoise,
                                 save_path,
