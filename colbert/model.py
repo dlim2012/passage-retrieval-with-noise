18b482aad5719888889cac157d63569491e27f19
dlim2012 <57515449+dlim2012@users.noreply.github.com>
2022-04-26 07:05:56 +0900

    Add files via upload
    Using Pytorch instead of the Pytorch Lightning package.
    Fixed query length to N_q = 32.

--- colbert/model.py
+++ colbert/model.py
@@ -1,82 +1,47 @@
+from transformers import BertModel
 import torch
-from transformers import BertModel, get_linear_schedule_with_warmup
-import pytorch_lightning as pl
-import numpy as np
 
-class ColBERT(pl.LightningModule):
-    def __init__(self,
-                 linear_scheduler_steps,
-                 B,
-                 tokenizer,
-                 vector_size=128,
-                 lr=3e-6,
-                 measure_steps=250,
-                 model_name='bert-base-uncased',
-                 early_stop=True):
-
-        super().__init__()
-        self.save_hyperparameters()
-
-        self.encoder = BertModel.from_pretrained(model_name)
+class ColBERT(torch.nn.Module):
+    def __init__(self, tokenizer, B, vector_size, device):
+        super(ColBERT, self).__init__()
+        self.tokenizer = tokenizer
+        self.encoder = BertModel.from_pretrained('bert-base-uncased')
         self.encoder.resize_token_embeddings(len(tokenizer))
 
-        self.linear_scheduler_steps = linear_scheduler_steps
-        self.B = B
-        self.tokenizer = tokenizer
-        self.lr = lr
-
-        self.labels = torch.cat((torch.eye(B), torch.zeros(B, B)), 1)
-        self.I = torch.eye(self.B)
         self.linear = torch.nn.Linear(in_features=768, out_features=vector_size, bias=True)
-
-        self.softmax = torch.nn.Softmax(dim=1)
-        self.labels = torch.arange(B)
-
-        # Monitor training steps for logging purpose
-        self.steps = torch.tensor(0).type(torch.float32)
-
-        # counts and step intervals for performance logging
-        self.counts = np.zeros(4) # n_correct, n_total, n_correct_epoch, n_total_epoch
-        self.loss_interval = 0
-        self.measure_steps = measure_steps
-
-        # For early stop
-        self.early_stop = early_stop
-        self.prev_loss_interval = 0
-        self.min_loss_interval = float('inf')
-        self.early_stop_count = 0
+        self.B = B
+        self.I = torch.eye(self.B)
 
 
-    def forward(self, x):
-        """
-        :param x: x['input_ids'] is the input to the BERT
-        """
+    def vector_representation(self, x):
 
-        #print(x['query_input_ids'].shape, x['query_attention_mask'].shape, x['positive_input_ids'].shape,
-        #      x['positive_attention_mask'].shape, x['negative_input_ids'].shape, x['negative_attention_mask'].shape)
+        x = self.encoder(**x).last_hidden_state # Shape: (B, token_len, 768)
 
-        query_vectors = torch.nn.functional.normalize(self.linear(self.encoder(
-            input_ids=x['query_input_ids'],
-            attention_mask=x['query_attention_mask']
-        ).last_hidden_state))    # shape: (B, q, d)
+        x = self.linear(x) # Shape: (B, token_len, vector_size)
 
-        positive_vectors = torch.nn.functional.normalize(self.linear(self.encoder(
-            input_ids=x['positive_input_ids'],
-            attention_mask=x['positive_attention_mask']
-        ).last_hidden_state))   # shape: (B, p1, d)
+        x = torch.nn.functional.normalize(x) # Normalize to calculate the cosine simliarity
 
-        negative_vectors = torch.nn.functional.normalize(self.linear(self.encoder(
-            input_ids=x['negative_input_ids'],
-            attention_mask=x['negative_attention_mask']
-        ).last_hidden_state))    # shape: (B, p2, d)
+        return x
+
+    def forward(self, batch):
+
+        # Calculate the vector representations of queries
+        query_vectors = self.vector_representation(batch['queries'])    # shape: (B, N_q + 3, d)
+
+        # Calculate the vector representations of positive passages
+        positive_vectors = self.vector_representation(batch['positives'])  # shape: (B, N_p + 3, d)
+
+        # Calculate the vector representations of negative passages
+        negative_vectors = self.vector_representation(batch['negatives'])   # shape: (B, N_p + 3, d)
 
         # Similarity scores
-        pos_scores = self.late_interaction(query_vectors, positive_vectors, x['positive_punc_mask']) # shape: (B, B)
-        neg_scores = self.late_interaction(query_vectors, negative_vectors, x['negative_punc_mask']) # shape: (B, B)
+        pos_scores = self.late_interaction(query_vectors, positive_vectors, batch['positive_filter_indices']) # shape: (B, B)
+        neg_scores = self.late_interaction(query_vectors, negative_vectors, batch['negative_filter_indices']) # shape: (B, B)
         scores = torch.cat((pos_scores, neg_scores), 1) # shape: (B, 2B)
+
         return scores
 
-    def late_interaction(self, query_vectors, passage_vectors, punc_mask):
+    def late_interaction(self, query_vectors, passage_vectors, filter_indices):
         scores = torch.zeros(query_vectors.shape[0], passage_vectors.shape[0])
 
         #for i in range(query_vectors.shape[0]):
@@ -86,18 +51,17 @@ class ColBERT(pl.LightningModule):
         for i in range(query_vectors.shape[0]):
             # Calculate cosine similarity scores between all query vectors and all passage vectors
             similarities = torch.matmul(query_vectors[i], torch.permute(passage_vectors, (0, 2, 1))).cpu()
-            # shape: (B, q, p)
+            # shape: (B, N_q, N_p)
             for j in range(passage_vectors.shape[0]):
                 # Filter punctuation vectors from passage vectors
                 # Calculate the maximum similarity for each query vector and sum the results
-                scores[i, j] = similarities[j][:, punc_mask[j]].max(1).values.sum()
-                # max shape: (B, q)
-                # shape: (B,)
+                scores[i, j] = similarities[j][:, filter_indices[j]].max(1).values.sum()
 
         return scores
 
     def loss(self, scores):
-        # Change data type to avoid having 0.0000e+00 for some of the softmax results
+
+        # Change data type to avoid having 0.0000e+00 for some of the softmax result
         scores = scores.type(torch.float64)
 
         # Calculate the probability of predicting correctly
@@ -110,109 +74,3 @@ class ColBERT(pl.LightningModule):
         loss = torch.mean(x)
 
         return loss
-
-
-    def on_train_batch_start(self, batch, batch_idx):
-        if self.early_stop and self.steps % self.measure_steps == 0:
-            if self.prev_loss_interval > self.min_loss_interval * 1.2:
-                self.early_stop_count += 1
-                if self.early_stop_count == 3:
-                    return -1
-            else:
-                self.early_stop_count = 0
-
-    def training_step(self, batch, batch_idx):
-        self.steps += 1
-
-        # Compute the forward path
-        scores = self.forward(batch)
-        loss = self.loss(scores)
-
-        # Add results to measurements
-        self.add_counts(scores)
-        self.loss_interval += loss.tolist()
-
-        # Save log
-        self.log('loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log('steps', self.steps, on_step=True, on_epoch=False, prog_bar=False, logger=True)
-
-        if self.steps % self.measure_steps == 0:
-            # Log performance periodically
-            accuracy = self.counts[0] / self.counts[1]
-            self.loss_interval /= self.measure_steps
-            self.prev_loss_interval = self.loss_interval
-            self.min_loss_interval = min(self.min_loss_interval, self.loss_interval)
-
-
-            self.log('acc', accuracy, on_step=True, on_epoch=False, prog_bar=True, logger=True)
-            self.log('loss_interval', self.loss_interval, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-            #print(accuracy, self.loss_interval)
-            self.counts[:2] = 0
-            self.loss_interval = torch.tensor(0).type(torch.float32)
-
-        # Return loss for backpropagation
-        return {'loss': loss}
-
-    def train_epoch_end(self, outputs):
-        # Log performance at the end of train epoch
-        accuracy = self.counts[0] / (self.counts[1] + 1e-10)
-        accuracy_epoch = self.counts[2] / self.counts[3]
-        self.log('acc', accuracy, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log('acc_end', accuracy_epoch, on_step=False, on_epoch=True, prog_bar=True, logger=True)
-        self.counts = np.zeros(4)
-
-    def validation_step(self, batch, batch_idx):
-
-        # Compute the forward path
-        scores = self.forward(batch)
-        loss = self.loss(scores)
-
-        # Save log
-        self.log('val_loss', loss, on_step=True, on_epoch=False, prog_bar=True, logger=True)
-
-    def validation_epoch_end(self, outputs):
-        # Log performance at the end of validation epoch
-        accuracy = self.counts[0] / (self.counts[1] + 1e-10)
-        accuracy_epoch = self.counts[2] / self.counts[3]
-        self.log('val_acc', accuracy, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log('val_acc_end', accuracy_epoch, on_step=False, on_epoch=True, prog_bar=True, logger=True)
-        self.counts = np.zeros(4)
-
-    def configure_optimizers(self):
-
-        # Using Adam optimizer
-        # lr=3e-6, beta1=0.9, beta2=0.999, L2_weight_decay=0.01
-        optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, self.parameters()), lr=self.lr,
-                                     weight_decay=0.01)
-
-        if not self.linear_scheduler_steps:
-            return [optimizer]
-
-        # learning rate warmup: ~self.linear_scheduler_steps[0] steps
-        # learning rate linear decay: self.linear_scheduler_steps[1] steps
-        schedulers = [
-            {
-                'scheduler': get_linear_schedule_with_warmup(
-                    optimizer,
-                    num_warmup_steps=self.linear_scheduler_steps[0],
-                    num_training_steps=self.linear_scheduler_steps[1]
-                ),
-                'name': 'warm_up_lr',
-                'interval': 'step'
-            }
-        ]
-
-        return [optimizer], schedulers
-
-    def add_counts(self, scores):
-        """
-        Calculate the number of correct predictions and add to measurements
-        """
-        prediction = torch.argmax(scores, axis=1).cpu()
-        n_correct = torch.sum((prediction == self.labels).type(torch.int32)).tolist()
-        n_total = scores.shape[0]
-
-        self.counts[[0, 2]] += n_correct
-        self.counts[[1, 3]] += n_total
-
-
--- colbert/train.py
+++ colbert/train.py
@@ -1,69 +1,59 @@
-"""
-v7: no linear
-v8: removed lr schedule, removed a for loop in loss, using punc_mask, batch size 24
-v9:
-todo: add linear
-"""
+import torch
+from torch.utils.data import Dataset, DataLoader
+import csv, random
+from transformers import BertTokenizer
+import os
+from tqdm import tqdm
+from tensorboardX import SummaryWriter
 import string
 
-from torch.utils.data import Dataset, DataLoader
-import torch
 from model import ColBERT
-from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
-import pytorch_lightning as pl
-from transformers import BertTokenizer
-import csv
-import os
 import argparse
 
 def parse():
     parser = argparse.ArgumentParser()
-    # learning rate
+    parser.add_argument('--version_name', type=str, required=True)  # ex. 0419_1_lr1e-6...
+
+    parser.add_argument('--train_data_file', type=str,
+                        default='/mnt/ssd/data/ms_marco/preprocessed/dpr/qidpidtriples.train.full.filtered.text.tsv')
+    parser.add_argument('--batch_size', type=int, default=24)
+    parser.add_argument('--train_steps', type=int, default=200000)
     parser.add_argument('--lr', type=float, default=3e-6)
-    parser.add_argument('--lr_schedule', default=False, action='store_true')
+    parser.add_argument('--not_lr_schedule', default=False, action='store_true')
+    parser.add_argument('--measure_steps', type=int, default=2000)
 
-    # good to use this argument in every training
-    parser.add_argument('--ckpt_dirname',  type=str, default='no_name')
-
-    # other arguments
-    parser.add_argument('--vector_size', type=float, default=128)
-    parser.add_argument('--early_stop', default=False, action='store_true')
-    parser.add_argument('--measure_steps', type=int, default=500)
-    parser.add_argument('--batch_size', type=int, default=16)
-
+    parser.add_argument('--vector_size', type=int, default=128) # padding length for queries
+    parser.add_argument('--N_q', type=int, default=32) # vector size for each token
     return parser.parse_args()
 
 
-qidpidtriples_file = 'data/ms_marco/preprocessed/dpr/qidpidtriples.train.full.filtered.text.tsv'
+args = parse()
 
-# model, tokenizer configuration
+
+#################### Data preprocessing
+
+# Tokenizer
 model_name = 'bert-base-uncased'
 tokenizer = BertTokenizer.from_pretrained(model_name)
 tokenizer.add_special_tokens({'additional_special_tokens': ['[Q]', '[D]']})
+
+# Token IDs
 Q_token_id, D_token_id = tokenizer.convert_tokens_to_ids(['[Q]', '[D]'])
-punctuation_ids = set(tokenizer.convert_tokens_to_ids([ch for ch in string.punctuation]))
+CLS_token_id, SEP_token_id, MASK_token_id, PAD_token_id = \
+    tokenizer.convert_tokens_to_ids(['[CLS]', '[SEP]', '[MASK]', '[PAD]'])
+punctuation_token_ids = set(tokenizer.convert_tokens_to_ids([ch for ch in string.punctuation]))
 
-# Parse arguments
-args = parse()
+assert CLS_token_id == 101
+assert SEP_token_id == 102
+assert MASK_token_id == 103
+assert PAD_token_id == 0
 
-# Number of training instances and linear scheduler_steps
-n_train_triples = args.batch_size * 200000
-if not args.lr_schedule:
-    linear_scheduler_steps = None
-else:
-    linear_scheduler_steps = (n_train_triples // (args.batch_size * 10),
-                              n_train_triples // args.batch_size + 1)
-
-
-# Directory to save checkpoints
-checkpoint_dir = os.path.join('checkpoints/colbert/', args.ckpt_dirname)
-
-# Directory to save logs
-log_dir = "log/colbert/"
-os.makedirs(log_dir, exist_ok=True)
-
+n_train_instances = args.batch_size * args.train_steps  # 12800000
 
 class TriplesDataset(Dataset):
+    """
+    A Dataset class made to use PyTorch DataLoader
+    """
     def __init__(self, inputs):
         self.data = inputs
 
@@ -73,113 +63,227 @@ class TriplesDataset(Dataset):
     def __getitem__(self, idx):
         return self.data[idx]
 
-
-def tokenize(texts, special_token_id):
-    n = len(texts)
+def tokenize(texts):
+    """
+    tokenize texts and return input data types
+    """
     inputs = tokenizer(texts, padding="longest", max_length=511, truncation=True)
-    input_ids, attention_mask = inputs["input_ids"], inputs["attention_mask"]
-    punctuation_mask = []
-    for i in range(n):
-        input_ids[i] = [input_ids[i][0]] + [special_token_id] + input_ids[i][1:]
+    return inputs['input_ids'], inputs['attention_mask'], inputs['token_type_ids']
+
+def tokenize_queries(texts):
+    input_ids, attention_mask, token_type_ids = tokenize(texts)
+
+    n_instances = len(texts)
+
+    for i in range(n_instances):
+        # length of query tokens excluding '[CLS]', '[SEP]', and '[PAD]' tokens
+        query_len = sum(attention_mask[i]) - 2
+
+        # Add '[Q]' token and truncate or pad query tokens with '[MASK]' tokens to N_q
+        if query_len > args.N_q:
+            query_tokens = input_ids[i][1: 1+args.N_q]
+        else:
+            query_tokens = input_ids[i][1: 1+query_len] + [MASK_token_id] * (args.N_q - query_len)
+        
+        # ('[CLS]', '[Q]', q0, q1, ..., ql, [MASK], [MASK], ..., [MASK], '[SEP]')
+        input_ids[i] = [CLS_token_id, Q_token_id] + query_tokens + [SEP_token_id]
+        attention_mask[i] = [1] * (args.N_q + 3)
+        token_type_ids[i] = [0] * (args.N_q + 3)
+        
+        assert len(input_ids[i]) == len(attention_mask[i])
+
+    inputs = {
+        'input_ids': torch.tensor(input_ids, dtype=torch.int32),
+        'attention_mask': torch.tensor(attention_mask, dtype=torch.int32),
+        'token_type_ids': torch.tensor(token_type_ids, dtype=torch.int32)
+    }
+
+    return inputs
+
+
+
+
+def tokenize_passages(texts):
+    n_instances = len(texts)
+
+    input_ids, attention_mask, token_type_ids = tokenize(texts)
+    filter_indices = [[] for _ in range(n_instances)]
+
+    for i in range(n_instances):
+        # Add '[D]' token
+        input_ids[i] = [CLS_token_id, D_token_id] + input_ids[i][1:]
         attention_mask[i] = [1] + attention_mask[i]
-        if special_token_id == Q_token_id:
-            continue
-        punctuation_mask.append([])
-        for j in range(len(input_ids[0])):
+        token_type_ids[i] = [0] + token_type_ids[i]
+
+        # Get the indices that are not paddings nor punctuations
+        for j in range(len(input_ids[i])):
+            # Stop at pad token
             if input_ids[i][j] == 0:
                 break
-            elif input_ids[i][j] in punctuation_ids:
-                continue
-            else:
-                punctuation_mask[-1].append(j)
-    return torch.tensor(input_ids), torch.tensor(attention_mask), punctuation_mask
+
+            # Append
+            if input_ids[i][j] not in punctuation_token_ids:
+                filter_indices[i].append(j)
+
+    inputs = {
+        'input_ids': torch.tensor(input_ids, dtype=torch.int32),
+        'attention_mask': torch.tensor(attention_mask, dtype=torch.int32),
+        'token_type_ids': torch.tensor(token_type_ids, dtype=torch.int32)
+    }
+
+    return inputs, filter_indices
 
 def collate_fn(data):
+    """
+    Convert query, positive passage, and negative passage to input data types
+    """
     x = dict()
-    x['query_input_ids'], x['query_attention_mask'], _ = \
-        tokenize([line[0] for line in data], Q_token_id)
-    x['positive_input_ids'], x['positive_attention_mask'], x['positive_punc_mask'] = \
-        tokenize([line[1] for line in data], D_token_id)
-    x['negative_input_ids'], x['negative_attention_mask'], x['negative_punc_mask'] = \
-        tokenize([line[2] for line in data], D_token_id)
+    x['queries'] = tokenize_queries([line[1] for line in data])
+    x['positives'], x['positive_filter_indices']= tokenize_passages([line[1] for line in data])
+    x['negatives'], x['negative_filter_indices'] = tokenize_passages([line[2] for line in data])
     return x
 
-def get_dataloader():
-    lines = []
-    with open(qidpidtriples_file, 'r') as f:
-        reader = csv.reader(f, delimiter='\t')
-        for i, line in enumerate(reader):
-            if i == n_train_triples:
-                break
-            lines.append(line)
-    dataset = TriplesDataset(lines)
-    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=10, collate_fn=collate_fn)
-    return dataloader
+
+def batch_to_device(batch, device):
+    for key, value in batch.items():
+        if type(value) == dict:
+            batch[key] = {key2: value2.to(device) for key2, value2 in value.items()}
+    return batch
+
+
+
+#################### Tools
+
+def linear_learning_rate_scheduler(optimizer, steps, target, warm_up, decay):
+    if steps < warm_up:
+        running_lr = target * steps / warm_up
+    else:
+        running_lr = target * (decay - steps) / (decay - warm_up)
+
+    for g in optimizer.param_groups:
+        g['lr'] = running_lr
+    return optimizer, running_lr
+
+
 
 def main():
+    #################### Get dataloader
+    lines = []
+    with open(args.train_data_file, 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        for i, line in enumerate(reader):
+            if i == n_train_instances:
+                break
+            lines.append(line)
 
-    # Declare a model
+    # Make a dataset
+    dataset = TriplesDataset(lines)
+
+    # Make a dataloader using collate_fn
+    # Use multiple processors and tokenize texts on the fly
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=10, collate_fn=collate_fn)
+
+
+    #################### Train
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using {device} device")
+
     model = ColBERT(
-        linear_scheduler_steps=linear_scheduler_steps,
-        B=args.batch_size,
         tokenizer=tokenizer,
-        lr=args.lr,
+        B=args.batch_size,
         vector_size=args.vector_size,
-        measure_steps=args.measure_steps,
-        model_name=model_name,
-        early_stop=args.early_stop
+        device=device
+    ).to(device)
+
+    # self.criterion = torch.nn.CrossEntropyLoss()
+    # optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=3e-6, weight_decay=0.01)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.01)
+
+    # Checkpoint directory
+    ckpt_dir = os.path.join('/mnt/ssd/checkpoints/colbert', args.version_name)
+    os.makedirs(ckpt_dir, exist_ok=True)
+    ckpt_name = os.path.join(ckpt_dir, 'steps=%d_loss-interval=%.4f.pth')
+    ckpt_info = [[None, float('inf')] for i in range(5)]
+
+    # Log directory
+    log_dir = os.path.join('log/colbert/pytorch_logs', args.version_name)
+    # writer = torch.utils.tensorboard.SummaryWriter(log_dir)
+    writer = SummaryWriter(log_dir)
+    os.makedirs(log_dir, exist_ok=True)
+
+    loss_intervals = []
+    accs = []
+    labels = torch.arange(args.batch_size)
+    for epoch in range(1):
+        running_loss = 0.0
+        running_accuracy = 0.0
+        running_lr = args.lr
+
+        pbar = tqdm(enumerate(dataloader), total=len(dataloader))
+        for i, data in pbar:
+
+            if not args.not_lr_schedule:
+                optimizer, running_lr = linear_learning_rate_scheduler(optimizer, i + 1, args.lr, args.train_steps / 10,
+                                                                       args.train_steps)
+
+            data = batch_to_device(data, device)
+
+            optimizer.zero_grad()
+            scores = model(data).cpu()
+            loss = model.loss(scores)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            running_accuracy += torch.sum((torch.argmax(scores, axis=1) == labels)\
+                                          .type(torch.int32)).detach().tolist()
+            print(running_loss, running_accuracy)
+
+            if (i + 1) % args.measure_steps == 0:
+                # model.train(False)
+
+                avg_loss = running_loss / args.measure_steps
+                avg_acc = running_accuracy / (args.batch_size * args.measure_steps)
+
+                # print('%d, %.4f, %.4f' % (i+1, avg_loss, avg_acc))
+                writer.add_scalar('loss', avg_loss, i)
+                writer.add_scalar('acc', avg_acc, i)
+
+                writer.add_scalar('lr', running_lr, i)
+                loss_intervals.append(avg_loss)
+                accs.append(avg_acc)
+
+                running_loss = 0.0
+                running_accuracy = 0.0
+                # writer.flush()
+
+                ckpt_info.sort(key=lambda x: x[1], reverse=True)
+                if avg_loss < ckpt_info[0][1]:
+                    if ckpt_info[0][0] != None:
+                        os.remove(ckpt_info[0][0])
+                    save_name = ckpt_name % (i + 1, avg_loss)
+                    torch.save(model, save_name)
+                    ckpt_info[0] = [save_name, avg_loss]
+
+                postfix = {'loss_interval': avg_loss, 'acc': avg_acc, 'lr': running_lr}
+                pbar.set_postfix(postfix)
+
+                # model.train(True)
+    save_name = ckpt_name % (i + 1, avg_loss)
+    torch.save(model, save_name)
+
+    writer.add_hparams(
+        {"lr": args.lr,
+         "not_lr_schedule": args.not_lr_schedule,
+         "train_steps": args.train_steps,
+         "batch_size": args.batch_size,
+         "train_data_file": args.train_data_file,
+         'ckpt_dir': args.ckpt_dir},
+        {'min_loss': min(loss_intervals),
+         'max_acc': max(accs)}
     )
-
-    # Use a TensorBoardLogger
-    logger = pl.loggers.TensorBoardLogger(save_dir=log_dir)
-
-    # Log learning rate
-    lr_monitor = LearningRateMonitor(logging_interval='step')
-
-    if args.ckpt_dirname != 'no_name':
-        # Save checkpoint: three lowest loss_interval
-        min_loss_checkpoint = ModelCheckpoint(
-            dirpath=checkpoint_dir,
-            filename="1_{steps:.0f}-{loss:.4e}-{loss_interval:.4e}-{acc:.4f}",
-            monitor="loss",
-            mode='min',
-            every_n_train_steps=100,
-            save_top_k=5,
-        )
-
-        min_loss_interval_checkpoint = ModelCheckpoint(
-            dirpath=checkpoint_dir,
-            filename="2_{steps:.0f}-{loss:.4e}-{loss_interval:.4e}-{acc:.4f}",
-            monitor="loss",
-            mode='min',
-            every_n_train_steps=100,
-            save_top_k=3,
-        )
-
-        # Save checkpoint after every epoch
-        epoch_checkpoint = ModelCheckpoint(
-            dirpath=checkpoint_dir,
-            filename="{epoch}-end",
-            monitor="steps",
-        )
-
-        callbacks = [min_loss_checkpoint, min_loss_interval_checkpoint, epoch_checkpoint, lr_monitor]
-    else:
-        callbacks = [lr_monitor]
-
-    # Train the model
-    trainer = pl.Trainer(
-        gpus=1,
-        max_epochs=1,
-        logger=logger,
-        callbacks=callbacks
-    )
-
-    # Get dataloader
-    train_dataloader = get_dataloader()
-
-    # Train the model
-    trainer.fit(model, train_dataloader)
+    writer.close()
 
 if __name__ == '__main__':
     main()
