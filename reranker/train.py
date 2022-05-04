be12911d3cf35abc7fd6eb945e313d95083a5fad
dlim2012 <57515449+dlim2012@users.noreply.github.com>
2022-05-04 16:09:33 -0400

    Update train.py
    Added validation.

--- reranker/train.py
+++ reranker/train.py
@@ -1,6 +1,6 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
-import csv, random
+import random
 from transformers import BertTokenizer
 import os
 from tqdm import tqdm
@@ -9,11 +9,13 @@ from tensorboardX import SummaryWriter
 from model import Reranker
 import argparse
 
+
 def parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--version_name', type=str, required=True) # ex. 0419_1_lr1e-6...
-    
-    parser.add_argument('--train_data_file', type=str, default='/mnt/ssd/data/ms_marco/original/triples.train.small.tsv')
+    parser.add_argument('--version_name', type=str, required=True)  # ex. 0419_1_lr1e-6...
+
+    parser.add_argument('--train_data_file', type=str,
+                        default='/mnt/ssd/data/ms_marco/preprocessed/triples.train.small.shuffled.tsv')
     parser.add_argument('--batch_size', type=int, default=24)
     parser.add_argument('--train_steps', type=int, default=100000)
     parser.add_argument('--npr', type=int, default=1)
@@ -21,20 +23,23 @@ def parse():
     parser.add_argument('--not_lr_schedule', default=False, action='store_true')
     parser.add_argument('--measure_steps', type=int, default=1000)
 
-    parser.add_argument('--collate_fn2', default=False, action='store_true') # default: collate_fn(No queries[:64])
+    parser.add_argument('--collate_fn2', default=False, action='store_true')  # default: collate_fn(No queries[:64])
     # Initial version was collate_fn2(queries[:64])
     return parser.parse_args()
 
-args = parse()
 
-#################### Dataloader
+args = parse()
 
 model_name = 'bert-base-uncased'
 tokenizer = BertTokenizer.from_pretrained(model_name)
 
-
 n_train_instances = args.batch_size * args.train_steps  # 12800000
+n_max_validation_instances = 5000
 
+validation_step = 200
+
+
+#################### Dataloader
 
 class qidpidlabelDataset(Dataset):
     """
@@ -63,10 +68,11 @@ def collate_fn(data):
         passages.append(passage)
         labels.append(label)
 
-    inputs = tokenizer(queries, passages, padding='longest', truncation=True, return_tensors='pt')
+    inputs = tokenizer(queries, passages, padding=True, truncation=True, return_tensors='pt')
     inputs['labels'] = torch.tensor(labels)
 
     return inputs
+
 
 def collate_fn_2(data):
     """
@@ -128,99 +134,119 @@ def linear_learning_rate_scheduler(optimizer, steps, target, warm_up, decay):
         g['lr'] = running_lr
     return optimizer, running_lr
 
-
+train_data_file = '/mnt/ssd/data/ms_marco/preprocessed/train.tsv'
+validation_data_file = '/mnt/ssd/data/ms_marco/preprocessed/validation.tsv'
 
 def main():
     #################### Get dataloader
     # Read training data
-    lines = []
-    with open(args.train_data_file, 'r') as f:
-        reader = csv.reader(f, delimiter='\t')
-        for i, line in enumerate(reader):
-            if len(lines) == n_train_instances:
+    train_data = []
+    with open(train_data_file, 'r') as f:
+        for i, line in enumerate(f):
+            line = line.split('\t')
+            if len(train_data) == n_train_instances:
                 break
             if random.random() < 1 / args.npr:
-                lines.append([line[0], line[1], 1])
-            if len(lines) == n_train_instances:
+                train_data.append([line[0], line[1], 1])
+            if len(train_data) == n_train_instances:
                 break
-            lines.append([line[0], line[2], 0])
+            train_data.append([line[0], line[2], 0])
+
+    validation_data = []
+    with open(validation_data, 'r') as f:
+        for i, line in enumerate(f):
+            line = line.split('\t')
+            if len(validation_data) == n_max_validation_instances:
+                break
+            if random.random() < 1 / args.npr:
+                validation_data.append([line[0], line[1], 1])
+            if len(validation_data) == n_max_validation_instances:
+                break
+            validation_data.append([line[0], line[2], 0])
+
 
     # Make a dataset
-    dataset = qidpidlabelDataset(lines)
+    train_dataset = qidpidlabelDataset(train_data)
+    validation_dataset = qidpidlabelDataset(validation_data)
 
     # Make a dataloader using collate_fn
     # Use multiple processors and tokenize texts on the fly
     if args.collate_fn2:
-        dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=10,
+        train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=10,
+                                collate_fn=collate_fn_2)
+        validation_dataloader = DataLoader(validation_dataset, batch_size=args.batch_size, shuffle=True, num_workers=10,
                                 collate_fn=collate_fn_2)
     else:
-        dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=10,
+        train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=10,
+                                collate_fn=collate_fn)
+        validation_dataloader = DataLoader(validation_dataset, batch_size=args.batch_size, shuffle=True, num_workers=10,
                                 collate_fn=collate_fn)
 
     #################### Train
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using {device} device")
-    
+
     model = Reranker().to(device)
-    
+
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.01)
-    
+
     # Save name
-    
+
     # Checkpoint directory
     ckpt_dir = os.path.join('/mnt/ssd/checkpoints/reranker', args.version_name)
     os.makedirs(ckpt_dir, exist_ok=True)
     ckpt_name = os.path.join(ckpt_dir, 'steps=%d_loss-interval=%.4f.pth')
     ckpt_info = [[None, float('inf')] for i in range(10)]
-    
+
     # Log directory
     log_dir = os.path.join('log/reranker/pytorch_logs', args.version_name)
-    #writer = torch.utils.tensorboard.SummaryWriter(log_dir)
+    # writer = torch.utils.tensorboard.SummaryWriter(log_dir)
     writer = SummaryWriter(log_dir)
     os.makedirs(log_dir, exist_ok=True)
-    
+
     loss_intervals = []
     accs = []
+    early_stop_count = 0
     for epoch in range(1):
         running_loss = 0.0
         running_accuracy = 0.0
         running_lr = args.lr
-    
-        pbar = tqdm(enumerate(dataloader), total=len(dataloader))
+
+        pbar = tqdm(enumerate(train_dataloader), total=len(train_dataloader))
         for i, data in pbar:
-    
+
             if not args.not_lr_schedule:
-                optimizer, running_lr = linear_learning_rate_scheduler(optimizer, i + 1, args.lr, args.train_steps / 10, args.train_steps)
-    
+                optimizer, running_lr = linear_learning_rate_scheduler(optimizer, i + 1, args.lr, args.train_steps / 10,
+                                                                       args.train_steps)
+
             data = {key: value.to(device) for key, value in data.items()}
             optimizer.zero_grad()
             outputs = model(data)
             loss = outputs.loss
             loss.backward()
             optimizer.step()
-    
+
             running_loss += loss.item()
             running_accuracy += torch.sum(
                 (torch.argmax(outputs.logits, dim=1) == data['labels']).type(torch.int32)).item() / len(data['labels'])
-    
+
             if (i + 1) % args.measure_steps == 0:
-                # model.train(False)
 
                 avg_loss = running_loss / args.measure_steps
                 avg_acc = running_accuracy / args.measure_steps
-    
+
                 # print('%d, %.4f, %.4f' % (i+1, avg_loss, avg_acc))
                 writer.add_scalar('loss', avg_loss, i)
                 writer.add_scalar('acc', avg_acc, i)
-    
+
                 writer.add_scalar('lr', running_lr, i)
                 loss_intervals.append(avg_loss)
                 accs.append(avg_acc)
-    
+
                 running_loss = 0.0
                 running_accuracy = 0.0
                 # writer.flush()
-    
+
                 ckpt_info.sort(key=lambda x: x[1], reverse=True)
                 if avg_loss < ckpt_info[0][1]:
                     if ckpt_info[0][0] != None:
@@ -228,15 +254,40 @@ def main():
                     save_name = ckpt_name % (i + 1, avg_loss)
                     torch.save(model, save_name)
                     ckpt_info[0] = [save_name, avg_loss]
-    
+
                 postfix = {'loss_interval': avg_loss, 'acc': avg_acc, 'lr': running_lr}
                 pbar.set_postfix(postfix)
-    
-                # model.train(True)
+
+                if avg_loss > sum([info[1] for info in ckpt_info]) / len(ckpt_info) * 1.1:
+                    early_stop_count += 1
+                    if early_stop_count == 5:
+                        break
+                else:
+                    early_stop_count = 0
+
+            if (i + 1) % validation_step == 0:
+                model.train(False)
+
+                for j, val_data in enumerate(validation_dataloader):
+                    val_data = {key: value.to(device) for key, value in val_data.items()}
+                    optimizer.zero_grad()
+                    outputs = model(val_data)
+                    loss = outputs.loss
+                    loss.backward()
+                    optimizer.step()
+
+                    running_loss += loss.item()
+                    running_accuracy += torch.sum(
+                        (torch.argmax(outputs.logits, dim=1) == val_data['labels']).type(torch.int32)).item() / len(
+                        val_data['labels'])
+
+                print(running_loss, running_accuracy)
+
+
+                model.train(True)
     save_name = ckpt_name % (i + 1, avg_loss)
     torch.save(model, save_name)
-    
-    
+
     writer.add_hparams(
         {"lr": args.lr,
          "not_lr_schedule": args.not_lr_schedule,
@@ -247,8 +298,9 @@ def main():
         {'min_loss': min(loss_intervals),
          'max_acc': max(accs)}
     )
-    
+
     writer.close()
-    
+
+
 if __name__ == '__main__':
     main()
